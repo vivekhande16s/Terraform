c08083033e40ce32491401230c43005072204508
vivekhande16s <82145355+vivekhande16s@users.noreply.github.com>
2021-04-16 13:46:41 +0530

    Delete vpc.tf

--- vpc.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "javahome_vpc" {
-  cidr_block       = "${var.vpc_cidr}"
-  instance_tenancy = "${var.tenancy}"
-  tags = {
-    Name = "${terraform.workspace}-javahome-vpc"
-  }
-}
